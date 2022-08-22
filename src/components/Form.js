b127053d383f5e7c4150f5bfc4931d57840b62a2
MishaDesign <82459404+MishaDesign@users.noreply.github.com>
2022-08-23 00:26:24 +0100

    react icons fix

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { collection, addDoc, getDocs } from 'firebase/firestore';
-import db from '../../pages/utils/firebaseConfig';
+import db from '../../utils/firebaseConfig';
 
 const Form = () => {
   const [formValid, setFormValid] = useState(false);
