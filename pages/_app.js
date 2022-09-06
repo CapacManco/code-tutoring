eccb04a041c018f03f50b2928226ba8e5b908e48
MishaDesign <82459404+MishaDesign@users.noreply.github.com>
2022-09-06 18:01:34 +0200

    fix design

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,14 @@ import '../public/style.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      ></meta>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
